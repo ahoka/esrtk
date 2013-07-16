5afed65fbb5dc3321dc91adc6c8a06d9d261a1d5
Adam Hoka <devnull@localhost>
2013-07-16 14:51:50 +0200

    ...

--- Platform/X86/PageDirectory.cc
+++ Platform/X86/PageDirectory.cc
@@ -1,40 +1,14 @@
 #include <X86/CpuRegisters.hh>
+#include <PageFrameAllocator.hh>
+#include <Parameters.hh>
 
-#ifdef PAE
-struct Entry
+class PageDirectory
 {
-   unsigned int low;
-   unsigned int high;
+   void init()
+   {
+      uint32_t* pageDirectory = (uint32_t* )PageFrameAllocator::getFreePage();
+      KASSERT(pageDirectory != 0);
+
+      memset(pageDirectory, 0, PageSize);
+   }
 };
-#else
-typedef unsigned long Entry;
-#endif
-
-char bitmap[4096];
-Entry entries[4096];
-
-Entry*
-getFreePageEntry()
-{
-   for (unsigned int i = 0; i < sizeof bitmap; i++)
-   {
-      if (bitmap[i] == 0)
-      {
-	 bitmap[i] = 1;
-	 return &entries[i];
-      }
-   }
-
-   return 0;
-}
-
-int
-initPageDirectory()
-{
-   for (unsigned int i = 0; i < sizeof bitmap; i++)
-   {
-      bitmap[i] = 0;
-   }
-
-   return 0;
-}
