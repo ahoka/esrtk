6acfe9e6471ee8ebec8c9b3cb5afb84d9805a5b2
Adam Hoka <devnull@localhost>
2013-09-24 18:13:05 +0200

    100hz timer interrupt

--- Platform/X86/Pit.cc
+++ Platform/X86/Pit.cc
@@ -1,12 +1,20 @@
 #include <Interrupt.hh>
+#include <Driver.hh>
+#include <Debug.hh>
+
+#include <X86/IoPort.hh>
 
 #include <cstdio>
 
-class Pit : InterruptHandler
+class Pit : InterruptHandler, Driver
 {
 public:
    Pit();
    ~Pit();
+
+   int probe();
+   bool init();
+   bool finalize();
 
    void handler(irq_t irq);
 
@@ -19,6 +27,11 @@ private:
       Command = 0x43
    };
 
+   enum
+   {
+      OscillatorFrequency = 1193182
+   };
+
    enum Command
    {
       SelectChannel0 = 0x0 << 6,
@@ -29,7 +42,9 @@ private:
       LatchLowByteOnly = 0x1 << 4,
       LatchHighByteOnly = 0x2 << 4,
       LatchBothBytes = 0x3 << 4,
-      InterruptOnTerminalCount = 0x0 << 1,
+      InterruptOnTerminalCountMode = 0x0 << 1,
+      SquareWaveMode = 0x1 << 1,
+      RateGeneratorMode = 0x2 << 1,
       BcdMode = 0x1 << 0
    };
 };
@@ -47,7 +62,41 @@ Pit::~Pit()
 void
 Pit::handler(irq_t /*irq*/)
 {
-   printf("PIT IRQ received\n");
+//   printf("PIT IRQ received\n");
+}
+
+int
+Pit::probe()
+{
+   return 1;
+}
+
+bool
+Pit::init()
+{
+   int hz = 100; // XXX
+   int freq = OscillatorFrequency / hz;
+
+   KASSERT((freq & ~0xffff) == 0);
+
+   outb(Command, SelectChannel0 | LatchBothBytes | RateGeneratorMode);
+
+   outb(Channel0, (uint8_t )freq);
+   outb(Channel0, (uint8_t )(freq >> 8));
+
+   Interrupt::registerHandler(0, this);
+   Interrupt::enableInterrupt(0);
+
+   return true;
+}
+
+bool
+Pit::finalize()
+{
+   Interrupt::disableInterrupt(0);
+   Interrupt::deregisterHandler(0, this);
+
+   return true;
 }
 
 Pit pit;
