09fa193b46b4bc878be7fd8ad83a270470cf8940
Adam Hoka <adam.hoka@gmail.com>
2013-10-22 00:14:32 +0200

    floppy image tool

---
+++ mkimage.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+if [ $# -lt 2 ]
+then
+    echo not enough arguments
+    exit 1
+fi
+
+if [ ! -e $1 ]
+then
+    echo invalid input
+    exit 1
+fi
+
+# TODO mktemp
+
+dd if=/dev/zero of=pad bs=1 count=750
+
+cat Loader/stage1 Loader/stage2 pad $1 > tmp.img
+
+pad2size=$((1440 * 1024 - $(du -b tmp.img | cut -f 1)))
+dd if=/dev/zero of=pad2 bs=1 count=${pad2size}
+
+cat tmp.img pad2 > $2
+
+rm pad tmp.img pad2
