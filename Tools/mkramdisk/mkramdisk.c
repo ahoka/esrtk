7ce62a3785c144192c093963c6e337a9f4e1bddb
Adam Hoka <devnull@localhost>
2013-11-04 16:00:06 +0100

    kinda works, needs proper error testing though

--- Tools/mkramdisk/mkramdisk.c
+++ Tools/mkramdisk/mkramdisk.c
@@ -102,9 +102,9 @@ main(int argc, char** argv)
    }
 
    char tmpfilename[PATH_MAX];
-   snprintf(tmpfilename, sizeof(tmpfilename), "%s/mkramdisk-XXXXXX", "/tmp");
-   int tmpfd = mkstemp(tmpfilename);
-   if (tmpfd == -1)
+   snprintf(tmpfilename, sizeof(tmpfilename), "%s/mkramdisk-superblock-XXXXXX", "/tmp");
+   int tmpfd_header = mkstemp(tmpfilename);
+   if (tmpfd_header == -1)
    {
       perror("Can't create tmp file");
       error = errno;
@@ -112,8 +112,27 @@ main(int argc, char** argv)
       goto out3;
    }
 
-   FILE *tmpfile = fdopen(tmpfd, "r+");
-   if (tmpfile == NULL)
+   snprintf(tmpfilename, sizeof(tmpfilename), "%s/mkramdisk-content-XXXXXX", "/tmp");
+   int tmpfd_content = mkstemp(tmpfilename);
+   if (tmpfd_content == -1)
+   {
+      perror("Can't create tmp file");
+      error = errno;
+      
+      goto out3;
+   }
+
+   FILE *tmpfile_header = fdopen(tmpfd_header, "r+");
+   if (tmpfile_header == NULL)
+   {
+      perror("Can't open tmp file");
+      error = errno;
+
+      goto out3;
+   }
+
+   FILE *tmpfile_content = fdopen(tmpfd_content, "r+");
+   if (tmpfile_content == NULL)
    {
       perror("Can't open tmp file");
       error = errno;
@@ -137,25 +156,41 @@ main(int argc, char** argv)
       if (strcmp(dent->d_name, ".") && strcmp(dent->d_name, ".."))
       {
          uint32_t size;
-         error = copy_file(ofile, dent->d_name, &size);
+         error = copy_file(tmpfile_content, dent->d_name, &size);
          if (error != 0)
          {
             break;
          }
 
          offset += size;
-         printf("ramdisk:%s (%lu)\n", dent->d_name, (unsigned long )size);
+         printf("ramdisk:%s (%lu:%lu)\n", dent->d_name, (unsigned long )offset, (unsigned long )size);
 
          memset(namebuf, 0, sizeof(namebuf));
          strcpy(namebuf, dent->d_name);
 
-         fwrite(namebuf, sizeof(namebuf), 1, tmpfile);
-         fwrite(&offset, sizeof(offset), 1, tmpfile);
-         fwrite(&size, sizeof(size), 1, tmpfile);
+         fwrite(namebuf, sizeof(namebuf), 1, tmpfile_header);
+         fwrite(&offset, sizeof(offset), 1, tmpfile_header);
+         fwrite(&size, sizeof(size), 1, tmpfile_header);
       }
    }
 
-   fclose(tmpfile);
+   // concatenate
+   char buffer[128];
+   int r;
+   fseek(tmpfile_header, 0L, SEEK_SET);
+   while ((r = fread(buffer, 1, sizeof(buffer), tmpfile_header)))
+   {
+      fwrite(buffer, r, 1, ofile);
+   }
+   
+   fseek(tmpfile_content, 0L, SEEK_SET);
+   while ((r = fread(buffer, 1, sizeof(buffer), tmpfile_content)))
+   {
+      fwrite(buffer, r, 1, ofile);
+   }
+
+   fclose(tmpfile_header);
+   fclose(tmpfile_content);
   out3:
    closedir(dir);
   out2:
