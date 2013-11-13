f0c9c2433f1a9f6a6268a0af06a0793918efd5b2
Adam Hoka <adam.hoka@gmail.com>
2013-11-13 18:17:53 +0100

    rework makefile

--- Tools/tools.mk
+++ Tools/tools.mk
@@ -4,15 +4,27 @@ CC=		clang
 COPTS+=		-O2 -Wall -Wextra -Werror
 
 CFLAGS+=	-std=c99
-CXXFLAGS+= 	-std=c++11
+CXXFLAGS+= 	-std=c++11 -fPIC
 
-all: $(PROG)
+OBJ=		$(SRC:.cc=.o)
+
+all: $(PROG) $(LIB) $(STATICLIB)
 
 %.o: %.cc
+	@echo Compiling object file $<
 	$(CXX) $(CXXFLAGS) -c -o $@ $<
 
-$(PROG): $(SRC:.cc=.o)
+$(PROG): $(OBJ)
+	@echo Linking application $<
 	$(CXX) $(LDFLAGS) $(LIBS) -o $(PROG) $^ $(STATICLIBS)
 
+$(LIB): $(OBJ)
+	@echo Linking dynamic library $<
+	$(CXX) -shared $(LDFLAGS) -o $(LIB) $(OBJ)
+
+$(STATICLIB): $(OBJ)
+	@echo Linking static library $<
+	ar rcs $(STATICLIB) $(OBJ)
+
 clean:
-	-rm -fr $(PROG) $(SRC:.cc=.o)
+	-rm -fr $(PROG) $(OBJ) $(LIB) $(STATICLIB)
