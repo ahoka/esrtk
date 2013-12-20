c6c0ce83441e2cb4a8ff03aec01e17acb870cdc7
Adam Hoka <adam.hoka@gmail.com>
2013-12-20 14:08:07 +0100

    Remove useless files

--- CLibrary/string/consttime_memequal.c.d
+++
@@ -1,4 +0,0 @@
-Library/string/consttime_memequal.o: Library/string/consttime_memequal.c \
-  Library/string/namespace.h /home/edmmhka/Code/kernel/Standard/string.h \
-  /home/edmmhka/Code/kernel/Standard/_cplusplus.h \
-  /home/edmmhka/Code/kernel/Standard/_size_t.h
--- CLibrary/string/ffs.c.d
+++
@@ -1,9 +0,0 @@
-Library/string/ffs.o: Library/string/ffs.c \
-  /home/edmmhka/Code/kernel/BsdCompat/sys/cdefs.h \
-  /home/edmmhka/Code/kernel/Standard/stddef.h \
-  /home/edmmhka/Code/kernel/Standard/_null.h \
-  /home/edmmhka/Code/kernel/Standard/_size_t.h \
-  /home/edmmhka/Code/kernel/BsdCompat/sys/types.h \
-  /home/edmmhka/Code/kernel/Standard/stdint.h \
-  /home/edmmhka/Code/kernel/BsdCompat/sys/bitops.h \
-  /home/edmmhka/Code/kernel/Standard/string.h
