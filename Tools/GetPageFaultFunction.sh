9aff31810151aa19ee793ac8c32e7d7deb443f0b
ahoka <devnull@localhost>
2013-12-25 15:45:19 +0100

    Helper to analize logs

---
+++ Tools/GetPageFaultFunction.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ -z $1 ]
+then
+        exit
+fi
+
+ADDR=$(grep -A 1 'Page fault:' $1  | tail -n 1 | cut -d " " -f 4)
+LINE=$(i686-elf-addr2line -e kernel.elf $ADDR)
+
+grep -A 1 'Page fault:' $1
+echo
+echo "Code line is:"
+echo $LINE
+
