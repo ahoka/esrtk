30835bc463fe7fcadde5c5769801bd93c59c8d4b
Adam Hoka <adam.hoka@gmail.com>
2014-02-28 09:45:41 +0100

    fix on linux (heh)

--- mkimage.sh
+++ mkimage.sh
@@ -18,7 +18,7 @@ dd if=/dev/zero of=pad bs=1 count=750
 
 cat Loader/stage1 Loader/stage2 pad $1 > tmp.img
 
-pad2size=$((1440 * 1024 - $(wc -c  tmp.img | tr -s ' ' | cut -d ' ' -f2)))
+pad2size=$((1440 * 1024 - $(wc -c < tmp.img | tr -s ' ' | cut -d ' ' -f2)))
 dd if=/dev/zero of=pad2 bs=1 count=${pad2size}
 
 cat tmp.img pad2 > $2
