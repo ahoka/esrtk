f62886f649c1238e2e27e1f221c99910112c9b92
Adam Hoka <adam.hoka@gmail.com>
2014-03-02 12:50:49 +0100

    Add an interrupt safe spinlock

--- Include/spinlock.h
+++ Include/spinlock.h
@@ -9,6 +9,12 @@ extern "C"
 typedef long spinlock_t;
 typedef long irq_flags_t;
 
+typedef struct spinlock_softirq
+{
+   spinlock_t lock;
+   irq_flags_t flags;
+} spinlock_softirq_t;
+
 #define SPINLOCK_STATIC_INITIALIZER 0
 
 long spinlock_enter(spinlock_t* lock);
@@ -19,8 +25,9 @@ long spinlock_try(spinlock_t* lock);
 // Disables local interrupts on lock, restores
 // on unlock.
 //
-long spinlock_softirq_enter(spinlock_t* lock, irq_flags_t* flags);
-long spinlock_softirq_exit(spinlock_t* lock, irq_flags_t flags);
+void spinlock_softirq_init(spinlock_softirq_t* lock);
+long spinlock_softirq_enter(spinlock_softirq_t* lock);
+long spinlock_softirq_exit(spinlock_softirq_t* lock);
 
 #ifdef __cplusplus
 }
---
+++ Platform/X86/SpinLock.cc
@@ -0,0 +1,29 @@
+#include <spinlock.h>
+#include <X86/Processor.hh>
+
+extern "C" void
+spinlock_softirq_init(spinlock_softirq_t* lock)
+{
+   lock->lock = SPINLOCK_STATIC_INITIALIZER;
+   lock->flags = 0;
+}
+
+extern "C" long
+spinlock_softirq_enter(spinlock_softirq_t* lock)
+{
+   lock->flags = get_eflags();
+   cli();
+
+   return spinlock_enter(&lock->lock);
+}
+
+extern "C" long
+spinlock_softirq_exit(spinlock_softirq_t* lock)
+{
+   if (lock->flags & InterruptEnable)
+   {
+      sti();
+   }
+
+   return spinlock_exit(&lock->lock);
+}
