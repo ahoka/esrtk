fccde1d471e2b7287a57b821b141f096d4eca234
Adam Hoka <adam.hoka@gmail.com>
2014-07-15 18:54:15 +0200

    Add vnode

---
+++ FileSystem/Node.hh
@@ -0,0 +1,23 @@
+#ifndef NODE_HH
+#define NODE_HH
+
+// represent a file node (vnode)
+
+class Node
+{
+public:
+   Node();
+   virtual ~Node();
+
+   virtual int open(int flags);
+   virtual int close();
+   virtual int read(void *, size_t);
+   virtual int write(const void*, size_t);
+   virtual int remove();
+
+private:
+   Node& operator=(const Node&) = delete;
+   Node(const Node&) = delete;
+};
+
+#endif
