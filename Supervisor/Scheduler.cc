30f7fcdbf157880d8b713428086b6dbdf5181f8e
Adam Hoka <ahoka@Adams-Mac.local>
2014-09-15 13:27:48 -0700

    Start a scheduler redesign

--- Supervisor/Scheduler.cc
+++ Supervisor/Scheduler.cc
@@ -3,13 +3,19 @@
 
 #include <cstdio>
 
+#include <ustl/uqueue.h>
+
 namespace Kernel
 {
    Thread thread0;
 
-   Thread* threads = 0;
+   using ThreadQueue = ustl::queue<Thread*>;
+   ThreadQueue* idleListM;
+   ThreadQueue* readyListM;
+
+//   Thread* threads = 0;
    Thread* currentThread = 0;
-   Thread* nextToRun = 0;
+//   Thread* nextToRun = 0;
 };
 
 using namespace Kernel;
@@ -17,7 +23,11 @@ using namespace Kernel;
 void
 Scheduler::init()
 {
+   readyListM = new ThreadQueue();
    thread0.init(0, StackStart);
+   printf("push\n");
+   readyListM->push(&thread0);
+   printf("push-\n");
 }
 
 void
@@ -35,25 +45,36 @@ Scheduler::getCurrentThread()
 void
 Scheduler::insert(Thread* t)
 {
+   printf("ins\n");
    // XXX
-   if (threads == 0) {
-      setCurrentThread(t);
-   }
+   // if (threads == 0)
+   // {
+   //    setCurrentThread(t);
+   // }
 
-   t->next = threads;
-   threads = t;
+//   t->next = threads;
+//   threads = t;
+   readyListM->push(t);
+
+   printf("ins-\n");
 }
 
 void
 Scheduler::schedule()
 {
-   if (nextToRun == 0)
-   {
-      nextToRun = threads;
-   }
+   // if (nextToRun == 0)
+   // {
+   //    nextToRun = threads;
+   // }
 
-   KASSERT(threads != 0);
+//   KASSERT(threads != 0);
 
-   currentThread = nextToRun;
-   nextToRun = nextToRun->next;
+   printf("sch\n");
+   currentThread = readyListM->front();
+   readyListM->pop();
+
+   // XXX no idle yet, just rr threads
+   readyListM->push(currentThread);
+//   currentThread = nextToRun;
+//   nextToRun = nextToRun->next;
 }
