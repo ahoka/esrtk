39b4a73b29d23881fbf71cef60dfbb1879673237
Adam Hoka <adam.hoka@gmail.com>
2014-11-02 11:44:22 +0100

    Add pci device structure and some limited pci debug code

--- Include/Pci.hh
+++ Include/Pci.hh
@@ -5,6 +5,27 @@
 
 class PciDriver;
 
+class PciDevice
+{
+public:
+   PciDevice(uint8_t bus, uint8_t device, uint8_t function);
+   PciDevice(const PciDevice& orig);
+   PciDevice& operator=(const PciDevice& orig);
+
+   uint8_t readConfigurationRegister8(uint8_t offset);
+   uint16_t readConfigurationRegister16(uint8_t offset);
+   uint32_t readConfigurationRegister32(uint8_t offset);
+
+   void writeConfigurationRegister8(uint8_t offset, uint8_t value);
+   void writeConfigurationRegister16(uint8_t offset, uint16_t value);
+   void writeConfigurationRegister32(uint8_t offset, uint32_t value);
+
+private:
+   uint8_t busM;
+   uint8_t deviceM;
+   uint8_t functionM;
+};
+
 namespace Pci
 {
    void init();
@@ -12,6 +33,8 @@ namespace Pci
    bool probeAndAttach(uint8_t bus, uint8_t device, uint8_t function);
    void registerDriver(PciDriver* driver);
    void enumerate();
+
+   void getDeviceInfo(PciDevice pciDevice);
 
    uint8_t readConfigurationRegister8(uint8_t bus, uint8_t device, uint8_t function, uint8_t offset);
    uint16_t readConfigurationRegister16(uint8_t bus, uint8_t device, uint8_t function, uint8_t offset);
--- Kernel/Common/Pci.cc
+++ Kernel/Common/Pci.cc
@@ -4,6 +4,91 @@
 
 static PciDriver* pciDrivers = 0;
 
+PciDevice::PciDevice(uint8_t bus, uint8_t device, uint8_t function)
+   : busM(bus),
+     deviceM(device),
+     functionM(function)
+{
+}
+
+PciDevice::PciDevice(const PciDevice& orig)
+   : busM(orig.busM),
+     deviceM(orig.deviceM),
+     functionM(orig.functionM)
+{
+}
+
+PciDevice&
+PciDevice::operator=(const PciDevice& orig)
+{
+   if (this != &orig)
+   {
+      busM = orig.busM;
+      deviceM = orig.deviceM;
+      functionM = orig.functionM;
+   }
+
+   return *this;
+}
+
+uint8_t
+PciDevice::readConfigurationRegister8(uint8_t offset)
+{
+   return Pci::readConfigurationRegister8(busM, deviceM, functionM, offset);
+}
+
+uint16_t
+PciDevice::readConfigurationRegister16(uint8_t offset)
+{
+   return Pci::readConfigurationRegister16(busM, deviceM, functionM, offset);
+}
+
+uint32_t
+PciDevice::readConfigurationRegister32(uint8_t offset)
+{
+   return Pci::readConfigurationRegister32(busM, deviceM, functionM, offset);
+}
+
+void
+PciDevice::writeConfigurationRegister8(uint8_t offset, uint8_t value)
+{
+   Pci::writeConfigurationRegister8(busM, deviceM, functionM, offset, value);
+}
+
+void
+PciDevice::writeConfigurationRegister16(uint8_t offset, uint16_t value)
+{
+   Pci::writeConfigurationRegister16(busM, deviceM, functionM, offset, value);
+}
+
+void
+PciDevice::writeConfigurationRegister32(uint8_t offset, uint32_t value)
+{
+   Pci::writeConfigurationRegister32(busM, deviceM, functionM, offset, value);
+}
+
+void
+Pci::getDeviceInfo(PciDevice pciDevice)
+{
+   uint8_t interruptPin = pciDevice.readConfigurationRegister8(Pci::Config::InterruptPin);
+   uint8_t interruptLine = pciDevice.readConfigurationRegister8(Pci::Config::InterruptLine);
+
+   printf("int: %u %u\n", interruptPin, interruptLine);
+
+   const char* pins[] = {
+      "None",
+      "INTA#",
+      "INTB#",
+      "INTC#",
+      "INTD#",
+   };
+
+   if (interruptPin != 0 && interruptPin <= 4)
+   {
+      printf("IRQ%u (%s)\n", interruptLine, pins[interruptPin]);
+   }
+}
+
 uint16_t
 Pci::getVendorId(uint8_t bus, uint8_t device, uint8_t function)
 {
@@ -80,6 +165,7 @@ Pci::enumerate()
 	    {
 	       uint16_t did = Pci::getDeviceId((uint8_t )bus, (uint8_t )dev, (uint8_t )fun);
                uint8_t deviceClass = readConfigurationRegister8((uint8_t )bus, (uint8_t )dev, (uint8_t )fun, 8);
+               getDeviceInfo(PciDevice(bus, dev, fun));
                if (!probeAndAttach(bus, dev, fun))
                {
                   printf("pci%x:%x:%x: Unknown %s: 0x%0hx:0x%0hx\n",
