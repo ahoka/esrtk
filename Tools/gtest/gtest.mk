9315b1ec04f618163f8bff20239c1c9ff0d0f2b1
Adam Hoka <adam.hoka@gmail.com>
2015-02-04 21:55:26 +0100

    Improve test support

--- Tools/gtest/gtest.mk
+++ Tools/gtest/gtest.mk
@@ -4,5 +4,14 @@
 GTESTPATH:=	$(abspath $(dir $(lastword $(MAKEFILE_LIST))))
 
 CXXFLAGS+=	-I$(GTESTPATH)/include
-STATICLIBS+=	-lgtest
+STATICLIBS+=	-lgtest -lgtest_main
 LDFLAGS+=	-L$(GTESTPATH) -lpthread
+
+TESTS_BIN=	${TESTS:.cc=.gtest}
+
+all: ${TESTS_BIN}
+	@echo TESTING
+	@echo ${TESTS_BIN}
+
+%.gtest: %.cc
+	${CXX} -Wall ${CXXFLAGS}  ${LDFLAGS} ${STATICLIBS} $^ -o $@
