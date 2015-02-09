13d247143dc22dd1c94cf8fe981213ce69f9eb8c
Adam Hoka <adam.hoka@gmail.com>
2015-02-09 13:19:27 +0100

    Local Apic Timer skeleton

---
+++ Kernel/X86/LocalApicTimer.cc
@@ -0,0 +1,24 @@
+#include <SystemTimer.hh>
+#include <Interrupt.hh>
+
+namespace Kernel
+{
+   namespace X86
+   {
+      class LocalApicTimer : InterruptHandler, SystemTimer
+      {
+      public:
+         LocalApicTimer();
+         ~LocalApicTimer();
+
+         int probe();
+         bool startTimer();
+         bool stopTimer();
+         
+         unsigned int getFrequency();
+         void delay(unsigned long ms);
+
+         void handleInterrupt();
+      };
+   };
+};
