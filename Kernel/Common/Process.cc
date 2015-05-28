f0b226de727d463d1607e49454e70bb91b7ce35c
Adam Hoka <adam.hoka@gmail.com>
2015-05-28 22:34:50 +0200

    cleanup

--- Kernel/Common/Process.cc
+++ Kernel/Common/Process.cc
@@ -7,9 +7,6 @@ using namespace Kernel;
 Process::Process()
    : contextM(new ProcessContext)
 {
-//   Thread* thread = Thread::createUserThread(this);
-//   threadsM.push_back(thread);
-//   thread->init();
 }
 
 Process::~Process()
