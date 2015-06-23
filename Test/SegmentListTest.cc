c7aa81189bf13e3b1d70af55ce1b97bd9055e89f
Adam Hoka <adam.hoka@gmail.com>
2015-06-23 18:01:42 +0200

    Add more tests for iteration

--- Test/SegmentListTest.cc
+++ Test/SegmentListTest.cc
@@ -25,5 +25,45 @@ SCENARIO("Kernel heap allocator SegmentList", "[kernelheap]")
             }
          }
       }
+
+      WHEN("An item is added")
+      {
+         Segment* s1 = new Segment();
+         s1->setSize(1);
+
+         list.add(s1);
+
+         AND_WHEN("Creating an iterator")
+         {
+            auto i = list.getIterator();
+
+            THEN("Iterator has a next item")
+            {
+               CHECK(i.hasNext() == true);
+
+               AND_THEN("The next item is the item added")
+               {
+                  CHECK(&i.getNext() == s1);
+
+                  AND_THEN("The iterator has no next item")
+                  {
+                     CHECK(i.hasNext() == false);
+                  }
+               }
+            }
+         }
+      }
+
+      WHEN("Three items are present")
+      {
+         Segment* s2 = new Segment();
+         Segment* s3 = new Segment();
+
+         s2->setSize(2);
+         s3->setSize(3);
+
+         list.add(s2);
+         list.add(s3);
+      }
    }
 }
