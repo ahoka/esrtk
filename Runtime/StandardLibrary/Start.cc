14b7c461264856ea2e77ecf460934e97ce549347
Adam Hoka <adam.hoka@gmail.com>
2015-06-29 16:51:21 +0200

    Try to force more section for ELF testing

--- Runtime/StandardLibrary/Start.cc
+++ Runtime/StandardLibrary/Start.cc
@@ -1,9 +1,26 @@
 #include "SystemCall.h"
 
+class Foo
+{
+public:
+   Foo(const char* text)
+      : t(text)
+   {
+   }
+
+   ~Foo()
+   {
+   }
+private:
+   const char* t;
+};
+
 extern "C" void
 start()
 {
    syscall(0, (void*)0x123456);
+
+   Foo foo("Hello");
 
    for (;;)
    {
