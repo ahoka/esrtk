22529c96673e0f8f06b3b26bfe1fc07849367369
Adam Hoka <adam.hoka@gmail.com>
2015-07-02 12:57:37 +0200

    Strip trailing ^M in logs

--- simulator.sh
+++ simulator.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-qemu-system-i386 -m 64M -M q35 -watchdog i6300esb -device rtl8139 -boot order=c -serial stdio -d cpu_reset -no-reboot -cdrom esrtk.iso 2>&1 | tee run.log
+qemu-system-i386 -m 64M -M q35 -watchdog i6300esb -device rtl8139 -boot order=c -serial stdio -d cpu_reset -no-reboot -cdrom esrtk.iso 2>&1 | tr -d '\r' | tee run.log
