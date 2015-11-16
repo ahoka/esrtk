6cfbfb16a797e1ca6f8521eea5cc1a00072d6db4
Adam Hoka <adam.hoka@gmail.com>
2015-11-16 22:57:48 +0100

    Use KVM

--- simulator.sh
+++ simulator.sh
@@ -2,4 +2,11 @@
 
 MEMORY=256M
 
-qemu-system-i386 -m ${MEMORY} -M q35 -watchdog i6300esb -device rtl8139 -boot order=c -serial stdio -d cpu_reset -no-reboot -cdrom esrtk.iso 2>&1 | tee run.log
+QEMU=qemu-system-i386
+if [ -x /usr/bin/kvm ]
+then
+    echo "Using KVM"
+    QEMU=kvm
+fi
+
+$QEMU -m ${MEMORY} -M q35 -watchdog i6300esb -device rtl8139 -boot order=c -serial stdio -d cpu_reset -no-reboot -cdrom esrtk.iso 2>&1 | tee run.log
