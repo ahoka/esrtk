9ba170b75d2ea44deac6f6ddd2f766a97693fb94
Adam Hoka <adam.hoka@gmail.com>
2015-11-18 23:03:52 +0100

    Support overriding emulator

--- simulator.sh
+++ simulator.sh
@@ -5,8 +5,25 @@ MEMORY=256M
 QEMU=qemu-system-i386
 if [ -x /usr/bin/kvm ]
 then
-    echo "Using KVM"
     QEMU=kvm
+fi
+
+if [ $# -eq 1 ]
+then
+    case $1 in
+        kvm)
+            echo "Using KVM"
+            QEMU=kvm
+            ;;
+        qemu)
+            echo "Using qemu"
+            QEMU=qemu-system-i386
+            ;;
+        *)
+            echo "Invalid emulator"
+            exit 1
+            ;;
+    esac
 fi
 
 $QEMU -m ${MEMORY} -M q35 -watchdog i6300esb -device rtl8139 -boot order=c -serial stdio -d cpu_reset -no-reboot -cdrom esrtk.iso 2>&1 | tee run.log
