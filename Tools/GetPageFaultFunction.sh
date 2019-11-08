1e53cda4c8101a5efac4b34f9f68ff5c9c5a2475
Adam Hoka <adam.hoka@gmail.com>
2019-11-08 12:49:56 +0100

    Disable FS module

--- Tools/GetPageFaultFunction.sh
+++ Tools/GetPageFaultFunction.sh
@@ -6,10 +6,9 @@ then
 fi
 
 ADDR=$(grep -m 1 -A 1 'Page fault:' $1  | tail -n 1 | cut -d " " -f 4)
-LINE=$(i686-elf-addr2line -e kernel.elf $ADDR)
+LINE=$(i686-elf-addr2line -e ./obj.i686/kernel.elf $ADDR)
 
 grep -m 1 -A 1 'Page fault:' $1
 echo
 echo "Code line is:"
 echo $LINE
-
